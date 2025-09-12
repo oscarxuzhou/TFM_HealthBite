949a303092d6e4d6171e616df7303965ca51ad3c
oxu <oscarxuz5@gmail.com>
2025-09-12 23:43:10 +0200

    Updating app

--- final app/HealthBite_app.py
+++ final app/HealthBite_app.py
@@ -15,41 +15,7 @@ from scipy import sparse
 from typing import List, Tuple, Optional
 import streamlit as st
 from app_key_functions import *
-
-
-# st.set_page_config(page_title="HealthBite", page_icon="🥗", layout="centered")
-# st.image('app images\HealthBite_image.png', use_column_width=True)
-# st.title("🥗 HealthBite")
-# st.caption("Un recomendador inteligente de recetas que también intenta ayudarte a sentir " \
-# "mejor a traves de una nutrición adecuada")
-
-# # ---------------- UI Streamlit ----------------
-# # En esta sección se recopila el código relacionado al UI de la app en sí
-# st.subheader("1) Sube una imagen de la nevera y describe cómo te sientes últimamente!")
-# user_text = st.text_area("Cómo te sientes últimamente?", placeholder="Por ejemplo: Últimamente me siento cansado constantemente")
-# photo = st.file_uploader("Sube la foto de tu nevera", placeholder="Detectaremos automáticamente los ingredientes que tienes disponibles!", type=["jpg","jpeg","png","webp"])
-
-# st.divider()
-
-# if st.button("Recommend", type="primary", use_container_width=True):
-#     # Load recipes
-#     if up:
-#         try:
-#             df = pd.read_csv(io.BytesIO(up.read()))
-#         except Exception as e:
-#             st.error(f"Could not read CSV: {e}")
-#             st.stop()
-#     else:
-#         df = demo_recipes()
-
-
-# HealthBite_app.py
-# HealthBite_app.py
-import io
 from pathlib import Path
-
-import pandas as pd
-import streamlit as st
 
 from app_key_functions import (
     NLP_YOLO_predictor_function,
